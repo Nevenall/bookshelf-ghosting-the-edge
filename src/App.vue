97409e57b0474e8efbd434f1e4125ab63e75ecab
Dan Behlings <nevenall@hotmail.com>
2018-04-24 13:09:06 -0700

    header fonts

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,41 @@
 <template>
-    <div id="app" class="page-container">
-        <md-app md-waterfall md-mode="fixed">
-            <md-app-toolbar class="md-primary">
-                <md-button class="md-icon-button" @click="showNavigation = true">
-                    <md-icon>menu</md-icon>
-                </md-button>
-                <span class="md-title">BookShelf
-                    <md-icon>keyboard_arrow_right</md-icon>{{book.title}}</span>
-            </md-app-toolbar>
-            <md-app-drawer :md-active.sync="showNavigation">
-                <md-toolbar class="md-primary" md-elevation="0">
-                    <img src="./assets/logo.png" alt="BookShelf">
-                </md-toolbar>
-                <md-list>
-                    <md-list-item @click="pushNav('./')">
-                        <h2>{{book.title}}</h2>
-                    </md-list-item>
-                    <md-list-item md-expand v-for="section in book.sections" :key="section.name">
-                        <span class="md-list-item-text">{{section.name}}</span>
-                        <md-list slot="md-expand">
-                            <md-list-item class="md-inset" v-for="nestedPage in section.pages" :key="nestedPage.path" @click="pushNav(nestedPage.path)">
-                                <span class="md-list-item-text">{{nestedPage.name}}</span>
-                            </md-list-item>
-                        </md-list>
-                    </md-list-item>
-                    <md-list-item v-for="page in book.pages" :key="page.path" @click="pushNav(page.path)">
-                        <span class="md-list-item-text">{{page.name}}</span>
-                    </md-list-item>
-                </md-list>
-            </md-app-drawer>
-            <md-app-content>
-                <div id="page">
-                    <router-view></router-view>
-                </div>
-            </md-app-content>
-        </md-app>
-    </div>
+   <div id="app" class="page-container">
+      <md-app md-waterfall md-mode="fixed">
+         <md-app-toolbar class="md-primary">
+            <md-button class="md-icon-button" @click="showNavigation = true">
+               <md-icon>menu</md-icon>
+            </md-button>
+            <span class="md-title">BookShelf
+               <md-icon>keyboard_arrow_right</md-icon>{{book.title}}</span>
+         </md-app-toolbar>
+         <md-app-drawer :md-active.sync="showNavigation">
+            <md-toolbar class="md-primary" md-elevation="0">
+               <img src="./assets/logo.png" alt="BookShelf">
+            </md-toolbar>
+            <md-list>
+               <md-list-item @click="pushNav('./')">
+                  <h2>{{book.title}}</h2>
+               </md-list-item>
+               <md-list-item md-expand v-for="section in book.sections" :key="section.name">
+                  <span class="md-list-item-text">{{section.name}}</span>
+                  <md-list slot="md-expand">
+                     <md-list-item class="md-inset" v-for="nestedPage in section.pages" :key="nestedPage.path" @click="pushNav(nestedPage.path)">
+                        <span class="md-list-item-text">{{nestedPage.name}}</span>
+                     </md-list-item>
+                  </md-list>
+               </md-list-item>
+               <md-list-item v-for="page in book.pages" :key="page.path" @click="pushNav(page.path)">
+                  <span class="md-list-item-text">{{page.name}}</span>
+               </md-list-item>
+            </md-list>
+         </md-app-drawer>
+         <md-app-content>
+            <div id="page">
+               <router-view></router-view>
+            </div>
+         </md-app-content>
+      </md-app>
+   </div>
 </template>
 
 <script>
@@ -89,7 +89,7 @@ export default {
 
 #page div {
   font-family: "IBM Plex Serif";
-  font-weight: 400;
+  font-weight: normal;
   max-width: 35em;
   margin: auto;
   font-size: 1rem;
@@ -113,11 +113,69 @@ export default {
   font-weight: normal;
 }
 
-/* 
-
-#page table {
-
+#page h1 {
+  font-size: 2.75rem; /* 44px */
+}
+#page h2 {
+  font-size: 2.375rem; /* 38px */
+}
+#page h3 {
+  font-size: 2.0625rem; /* 33px */
+}
+#page h4 {
+  font-size: 1.5625rem; /* 25px */
+}
+#page h5 {
+  font-size: 1.3125rem; /* 21px */
+}
+#page h6 {
+  font-size: 1rem;      /* 16px */
 }
 
-*/
+@media screen and (min-width: 60em) {
+  #page h1 {
+    font-size: 4.1875rem;  /* 67px */
+  }
+  #page h2 {
+    font-size: 3.1875rem;  /* 51px */
+  }
+  #page h3 {
+    font-size: 2.75rem;    /* 44px */
+  }
+  #page h4 {
+    font-size: 2.0625rem;  /* 33px */
+  }
+  #page h5 {
+    font-size: 1.5625rem;  /* 25px */
+  }
+  #page h6 {
+    font-size: 1.125rem;   /* 18px */
+  }
+  #page p {
+    font-size: 1.125rem;  /* 18px */
+  }
+}
+@media screen and (min-width: 120em) {
+  #page h1 {
+    font-size: 5.625rem; /* 90px */
+  }
+  #page h2 {
+    font-size: 4.1875rem; /* 67px */
+  }
+  #page h3 {
+    font-size: 3.1875rem;  /* 51px */
+  }
+  #page h4 {
+    font-size: 2.375rem; /* 38px */
+  }
+  #page h5 {
+    font-size: 1.75rem;   /* 28px */
+  }
+  #page h6 {
+    font-size: 1.3125rem;     /* 21px */
+  }
+  #page p {
+    font-size: 1.3125rem; /* 21px */
+  }
+}
 </style>
