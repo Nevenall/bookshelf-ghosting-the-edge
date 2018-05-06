963a0de2bb35ca1bdf75f1c92a8495264b5cffce
Dan Behlings <nevenall@hotmail.com>
2018-05-06 16:59:24 -0700

    Custom Raleway font files

---
+++ font-tool-commands.ps1
@@ -0,0 +1,14 @@
+pyftsubset Raleway-Bold.ttf --glyphs=* --layout-features=salt,lnum,smcp,liga,kern --flavor=woff2 --output-file=Raleway-Bold.woff2
+pyftsubset Raleway-BoldItalic.ttf --glyphs=* --layout-features=salt,lnum,smcp,liga,kern --flavor=woff2 --output-file=Raleway-BoldItalic.woff2
+pyftsubset Raleway-Italic.ttf --glyphs=* --layout-features=salt,lnum,smcp,liga,kern --flavor=woff2 --output-file=Raleway-Italic.woff2
+pyftsubset Raleway-Light.ttf --glyphs=* --layout-features=salt,lnum,smcp,liga,kern --flavor=woff2 --output-file=Raleway-Light.woff2
+pyftsubset Raleway-LightItalic.ttf --glyphs=* --layout-features=salt,lnum,smcp,liga,kern --flavor=woff2 --output-file=Raleway-LightItalic.woff2
+pyftsubset Raleway-Regular.ttf --glyphs=* --layout-features=salt,lnum,smcp,liga,kern --flavor=woff2 --output-file=Raleway-Regular.woff2
+
+
+pyftsubset Raleway-Bold.ttf --glyphs=* --layout-features=salt,lnum,smcp,liga,kern --flavor=woff --output-file=Raleway-Bold.woff
+pyftsubset Raleway-BoldItalic.ttf --glyphs=* --layout-features=salt,lnum,smcp,liga,kern --flavor=woff --output-file=Raleway-BoldItalic.woff
+pyftsubset Raleway-Italic.ttf --glyphs=* --layout-features=salt,lnum,smcp,liga,kern --flavor=woff --output-file=Raleway-Italic.woff
+pyftsubset Raleway-Light.ttf --glyphs=* --layout-features=salt,lnum,smcp,liga,kern --flavor=woff --output-file=Raleway-Light.woff
+pyftsubset Raleway-LightItalic.ttf --glyphs=* --layout-features=salt,lnum,smcp,liga,kern --flavor=woff --output-file=Raleway-LightItalic.woff
+pyftsubset Raleway-Regular.ttf --glyphs=* --layout-features=salt,lnum,smcp,liga,kern --flavor=woff --output-file=Raleway-Regular.woff
