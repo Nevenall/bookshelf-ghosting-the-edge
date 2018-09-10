19fbc5bed7293d37b666c4156a8d242981d54245
Dan Behlings <nevenall@hotmail.com>
2018-09-10 13:13:45 -0700

    page assets working removed uneeded fate font settings

--- vue.config.js
+++ vue.config.js
@@ -33,12 +33,22 @@ module.exports = {
          .use('vue-page-loader')
          .loader('./src/vue-page-loader')
 
-      config.module.
-      rule('fonts')
+
+      config.module
+         .rule('page-assets')
+         .include
+         .add(path.join(__dirname, 'src/pages/assets'))
+         .end()
          .use('url-loader')
          .loader('url-loader')
          .options({
-            limit: 20
+            limit: 4096,
+            fallback: {
+               loader: 'file-loader',
+               options: {
+                  name: 'assets/[name].[ext]'
+               }
+            }
          })
    }
 }
