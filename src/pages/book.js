4000eca62b7a971f58831882e2c2b0fa91d1c827
nevenall <nevenall@hotmail.com>
2023-09-22 13:30:26 -0700

    pages and stuff

--- src/pages/book.js
+++ src/pages/book.js
@@ -1,153 +1,19 @@
-module.exports = {
-   "title": "Ghosting the Edge",
-   "root": "C:\\src\\ghosting-the-edge\\html",
-   "allPages": [
-      {
-         "path": "01 Ghosting the Edge.html",
-         "order": 1,
-         "data": {
-            "name": "01 Ghosting the Edge",
-            "order": 1,
-            "sourcePath": "C:\\src\\ghosting-the-edge\\src\\01 Ghosting the Edge.md"
-         }
-      },
-      {
-         "path": "02 The Usual Suspects.html",
-         "order": 1,
-         "data": {
-            "name": "02 The Usual Suspects",
-            "order": 1,
-            "sourcePath": "C:\\src\\ghosting-the-edge\\src\\02 The Usual Suspects.md"
-         }
-      },
-      {
-         "path": "03 Gearing Up.html",
-         "order": 1,
-         "data": {
-            "name": "03 Gearing Up",
-            "order": 1,
-            "sourcePath": "C:\\src\\ghosting-the-edge\\src\\03 Gearing Up.md"
-         }
-      },
-      {
-         "path": "04 Rules to Break.html",
-         "order": 4,
-         "data": {
-            "name": "04 Rules to Break",
-            "order": 4,
-            "sourcePath": "C:\\src\\ghosting-the-edge\\src\\04 Rules to Break.md"
-         }
-      },
-      {
-         "path": "05 Running the Edge.html",
-         "order": 4,
-         "data": {
-            "name": "05 Running the Edge",
-            "order": 4,
-            "sourcePath": "C:\\src\\ghosting-the-edge\\src\\05 Running the Edge.md"
-         }
-      },
-      {
-         "path": "06 Afterwards.html",
-         "order": 4,
-         "data": {
-            "name": "06 Afterwards",
-            "order": 4,
-            "sourcePath": "C:\\src\\ghosting-the-edge\\src\\06 Afterwards.md"
-         }
-      },
-      {
-         "path": "Example Page.html",
-         "order": 5,
-         "data": {
-            "name": "Example Page",
-            "order": 5,
-            "sourcePath": "C:\\src\\ghosting-the-edge\\src\\Example Page.md"
-         }
-      },
-      {
-         "path": "readme.html",
-         "order": 6,
-         "data": {
-            "name": "readme",
-            "order": 6,
-            "sourcePath": "C:\\src\\ghosting-the-edge\\src\\readme.md"
-         }
-      }
-   ],
-   "sections": [],
-   "pages": [
-      {
-         "path": "01 Ghosting the Edge.html",
-         "order": 1,
-         "data": {
-            "name": "01 Ghosting the Edge",
-            "order": 1,
-            "sourcePath": "C:\\src\\ghosting-the-edge\\src\\01 Ghosting the Edge.md"
-         }
-      },
-      {
-         "path": "02 The Usual Suspects.html",
-         "order": 1,
-         "data": {
-            "name": "02 The Usual Suspects",
-            "order": 1,
-            "sourcePath": "C:\\src\\ghosting-the-edge\\src\\02 The Usual Suspects.md"
-         }
-      },
-      {
-         "path": "03 Gearing Up.html",
-         "order": 1,
-         "data": {
-            "name": "03 Gearing Up",
-            "order": 1,
-            "sourcePath": "C:\\src\\ghosting-the-edge\\src\\03 Gearing Up.md"
-         }
-      },
-      {
-         "path": "04 Rules to Break.html",
-         "order": 4,
-         "data": {
-            "name": "04 Rules to Break",
-            "order": 4,
-            "sourcePath": "C:\\src\\ghosting-the-edge\\src\\04 Rules to Break.md"
-         }
-      },
-      {
-         "path": "05 Running the Edge.html",
-         "order": 4,
-         "data": {
-            "name": "05 Running the Edge",
-            "order": 4,
-            "sourcePath": "C:\\src\\ghosting-the-edge\\src\\05 Running the Edge.md"
-         }
-      },
-      {
-         "path": "06 Afterwards.html",
-         "order": 4,
-         "data": {
-            "name": "06 Afterwards",
-            "order": 4,
-            "sourcePath": "C:\\src\\ghosting-the-edge\\src\\06 Afterwards.md"
-         }
-      },
-      {
-         "path": "Example Page.html",
-         "order": 5,
-         "data": {
-            "name": "Example Page",
-            "order": 5,
-            "sourcePath": "C:\\src\\ghosting-the-edge\\src\\Example Page.md"
-         }
-      },
-      {
-         "path": "readme.html",
-         "order": 6,
-         "data": {
-            "name": "readme",
-            "order": 6,
-            "sourcePath": "C:\\src\\ghosting-the-edge\\src\\readme.md"
-         }
-      }
-   ]
-}
+import Page0 from './Cover.html'
+import Page1 from './README.html'
+import Page2 from './01 Ghosting the Edge.html'
+import Page3 from './02 The Usual Suspects.html'
+import Page4 from './03 Gearing Up.html'
+import Page5 from './04 Rules to Break.html'
+import Page6 from './05 Running the Edge.html'
+import Page7 from './06 Afterwards.html'
+
+export default [
+   { title: 'Cover', path: '/', page: Page0 },
+   { title: 'Introduction', path: '/intro', page: Page1 },
+   { title: 'Ghosting the Edge', path: '/ghosting-the-edge', page: Page2 },
+   { title: 'The Usual Suspects', path: '/the-usual-suspects', page: Page3 },
+   { title: 'Gearing Up', path: '/gearing-up', page: Page4 },
+   { title: 'Rules to Break', path: '/rules-to-break', page: Page5 },
+   { title: 'Running the Edge', path: '/running-the-edge', page: Page6 },
+   { title: 'Afterwards', path: '/afterwards', page: Page7 },
+]
